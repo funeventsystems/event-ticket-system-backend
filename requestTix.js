2192ae715af9577c25ea00f5a1d95bbb4f36ffc3
gmanandmarbles <119905567+gmanandmarbles@users.noreply.github.com>
2023-10-12 16:44:36 -0600

    Update requestTix.js

--- requestTix.js
+++ requestTix.js
@@ -5,7 +5,9 @@ const nodemailer = require('nodemailer');
 const app = express();
 const port = 3000; // You can choose any port you like
 
-app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 // Serve static files (like your HTML page)
 app.use(express.static('public'));
@@ -19,51 +21,7 @@ pass: secrets.password, // Use the password from secrets.json
 },
 });
 
-app.post('/purchase', (req, res) => {
-  // Process the form data here
-  const { name, email, phone, standardTickets, vipTickets, childTickets } = req.body;
 
-  const data = {
-    name,
-    email,
-    phone,
-    standardTickets,
-    vipTickets,
-    childTickets,
-  };
-
-  // Save data to a JSON file (customize the file path)
-  const dataFilePath = 'data.json';
-  fs.readFile(dataFilePath, (err, fileData) => {
-    if (err) {
-      const jsonData = [data];
-      fs.writeFileSync(dataFilePath, JSON.stringify(jsonData));
-    } else {
-      const jsonData = JSON.parse(fileData);
-      jsonData.push(data);
-      fs.writeFileSync(dataFilePath, JSON.stringify(jsonData));
-    }
-  });
-
-  // Send an email to admin (customize recipient address)
-  const mailOptions = {
-    from: secrets.email,
-    to: 'malik@mastermindsyyc.xyz', // Change this to the admin's email
-    subject: 'New Ticket Purchase',
-    text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nStandard Tickets: ${standardTickets}\nVIP Tickets: ${vipTickets}\nChild Tickets: ${childTickets}`,
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log('Email not sent:', error);
-    } else {
-      console.log('Email sent:', info.response);
-    }
-  });
-
-  // Respond with a confirmation or redirect to a thank you page
-  res.send('Thank you for your purchase!');
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
